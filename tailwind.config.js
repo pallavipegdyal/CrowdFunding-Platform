87e996c11e75aa95ab135d68af4131659b690391
pallavipegdyal <147926556+pallavipegdyal@users.noreply.github.com>
2025-08-05 16:40:19 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
+
+
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust paths based on your project structure
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
